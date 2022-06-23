354e5d880ffdef97fa48ae44801b27c4eb320cf0
harman.singh <harman.singh@student.howest.be>
2022-06-23 18:42:50 +0200

    Initial commit after rebranch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Intro from './components/Intro';
+import About from './components/About';
+import Inline from './components/Inline';
+import Project from './components/Project';
+import Footer from './components/Footer';
+import Simple from './components/Simple';
+import SectionHeader from './components/SectionHeader';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Intro></Intro>
+        <SectionHeader section="ABOUT" title="about" desc="Find out more about me!"/>
+        <About/>
+        <SectionHeader section="PROJECTS" title="projects" desc="Here are some of my projects!"/>
+        <Inline/>
+        {/* <Project></Project> */}
+        {/* <Simple></Simple> */}
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,68 @@
+import MaterialIconsReact from "material-icons-react";
+// https://fonts.google.com/icons
+
+export default function About() {
+    return (
+        <div className="flex flex-wrap justify-center min-h-screen mt-24">
+            <div className="text-gray-700 rounded-md shadow-2xl p-8 m-5 border-4 border-t-orange-600 w-screen max-w-xl h-fit space-y-4">
+                <h2 className="font-bold text-xl pb-4">Personal Experience</h2>
+                <p className="pb-4">I started programming in May of 2020, after find out about Harvard's CS50. I was immediately hooked on coding, and decided right then and there I wanted to do this for a living. I got extremely lucky that I could turn my hobby into a profession, and that there's a specific University Degree for it.</p>
+                <p className="pb-4">I'm a first-year Computer Science student at HOWEST University of Applied Sciences. In my second year I will choose to specialize in <strong>Artificial Intelligence</strong>. I am extremely interested in this field and the opportunities it will give me.</p>
+                <p className="pb-4">I have an (for the time being) empty blog on Dev Community, which you can visit by clicking <a className="text-blue-600 hover:text-purple-600 font-semibold" href="https://dev.to/harmxn" target="_blank" rel="noreferrer">here</a>.</p>
+                <div className="float-right">
+                    <MaterialIconsReact icon="school" size="large"/>
+                </div>
+            </div>
+            
+            <div className="text-gray-700 rounded-md shadow-2xl p-8 m-5 border-4 border-t-purple-600 w-screen max-w-xl h-fit space-y-4">
+                <h2 className="font-bold text-xl pb-4">Skills</h2>
+                <p className="font-medium text-gray-700 text-md">Front End</p>
+                <div className="flex gap-2 flex-wrap">
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">HTML / CSS</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Responsive Design</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">JavaScript</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">TypeScript</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">React</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Tailwind CSS</p>
+                </div>
+
+                <p className="font-medium text-gray-700 text-md">Back End</p>
+                <div className="flex gap-2 flex-wrap">
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Java (Vert.x)</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">PHP (Laravel)</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Databases (MySQL)</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Unit Tests</p>
+                </div>
+
+                <p className="font-medium text-gray-700 text-md">Other</p>
+                <div className="flex gap-2 flex-wrap">
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Java (OOP)</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">REST API</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Python</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">C#</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Git Bash</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">GitHub / GitLab</p>
+                </div>
+
+                <p className="font-medium text-gray-700 text-md">Design Tools</p>
+                <div className="flex gap-2 flex-wrap">
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Figma</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Adobe XD</p>
+                    <p className="bg-gray-200 rounded p-2 text-xs font-medium hover:scale-105 ease-in-out duration-300">Lucid Chart</p>
+                </div>
+
+
+
+                <p className="pb-5 pt-5">If you'd like to find out even more, my resume is linked below.</p>
+                <div className="animate-pulse">
+                    <a className="text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-lg p-2 " 
+                    href="https://resume.io/r/SU1DQos8n" target="_blank" rel="noreferrer">Resume</a>
+                </div>
+                
+                <div className="float-right">
+                    <MaterialIconsReact icon="construction" size="large"/>
+                </div>
+            </div>
+        </div>
+        )
+    }
