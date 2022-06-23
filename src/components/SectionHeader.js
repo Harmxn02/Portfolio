59a23abefb4c0fad89359a78788cd89442bcd416
harman.singh <harman.singh@student.howest.be>
2022-06-23 19:16:33 +0200

    Add max-width to section headers

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -2,7 +2,7 @@
 
 export default function SectionHeader(props) {
     return (
-        <div id={props.section} className='flex flex-col mt-64 py-6 bg-gradient-to-r from-orange-500 to-purple-500'>
+        <div id={props.section} className='flex flex-col mt-64 py-6 bg-gradient-to-r from-orange-500 to-purple-500 max-w-max px-16 mx-auto'>
             <h1 className="text-5xl font-bold   text-white text-center mb-5">{props.title}</h1>
             <h2 className='text-lg              text-white text-center'>{props.desc}</h2>
         </div>
