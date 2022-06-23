f5847345c9d1f354450213f4ff80e64c6c933434
harman.singh <harman.singh@student.howest.be>
2022-06-23 19:54:39 +0200

    Changed font-weight so its readable on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ export default function Footer(props) {
     <div className="bg-white" id={props.section}>
       <header className="relative bg-white ">
         <div className="bg-gradient-to-r from-purple-500 to-blue-500 h-10 flex flex-col items-center justify-center text-white py-24 px-4 sm:px-6 lg:px-8">
-          <p  className='text-xl font-medium'>Here are some of my socials + email address</p>
-          <p  className='text-xl font-thin pb-4'>Feel free to contact me</p>
+          <p  className='text-xl text-center font-medium'>Here are some of my socials + email address</p>
+          <p  className='text-xl text-center font-light pb-4'>Feel free to contact me</p>
           <div className='flex flex-row gap-8 py-4'>
             <a href='https://www.linkedin.com/in/harmanpnahal/'   target="_blank" rel="noreferrer" ><FaLinkedin className='text-xl sm:text-2xl'/></a>
             <a href='https://github.com/Harmxn02/'                target="_blank" rel="noreferrer" ><FaGithub   className='text-xl sm:text-2xl'/></a>
