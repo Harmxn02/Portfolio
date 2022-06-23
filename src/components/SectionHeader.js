acf6c52764e9c9318db3d08e7994f0bbb0fedf1b
harman.singh <harman.singh@student.howest.be>
2022-06-23 22:00:47 +0200

    Change section-header height on mobile view

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -3,8 +3,8 @@
 export default function SectionHeader(props) {
     return (
         <div id={props.section} className='flex flex-col mt-64 py-6 bg-gradient-to-r from-orange-500 to-purple-500 px-16 min-w-screen sm:max-w-max sm:mx-auto'>
-            <h1 className="text-5xl font-bold   text-white text-center mb-5">{props.title}</h1>
-            <h2 className='text-lg              text-white text-center'>{props.desc}</h2>
+            <h1 className="text-xl sm:text-5xl font-bold   text-white text-center sm:mb-5">{props.title}</h1>
+            <h2 className='text-sm sm:text-lg              text-white text-center'>{props.desc}</h2>
         </div>
     )
 }
